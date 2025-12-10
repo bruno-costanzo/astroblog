c082fee13b1f14f194482c1ea7752ce5d8c7713c
Bruno Costanzo <dev.bcostanzo@gmail.com>
2025-12-10 13:56:40 +0100

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://brunorb.netlify.app",
+  site: "https://costanzo.blog",
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
